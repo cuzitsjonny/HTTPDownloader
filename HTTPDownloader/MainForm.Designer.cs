ebdf39e4a23276883b266a08d502206f1555615f
cuzitsjonny (Surface) <jonidongo@gmail.com>
2018-05-14 18:00:27 +0200

    Initial commit

---
+++ HTTPDownloader/MainForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace HTTPDownloader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this._DownloadButton = new System.Windows.Forms.Button();
+            this._SourceLabel = new System.Windows.Forms.Label();
+            this._DestinationLabel = new System.Windows.Forms.Label();
+            this._ProgressBar = new System.Windows.Forms.ProgressBar();
+            this._DestinationButton = new System.Windows.Forms.Button();
+            this._ProgressLabel = new System.Windows.Forms.Label();
+            this._SourceField = new System.Windows.Forms.RichTextBox();
+            this._DestinationField = new System.Windows.Forms.RichTextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this._DownloadButton, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this._SourceLabel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this._DestinationLabel, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this._ProgressBar, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this._DestinationButton, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this._ProgressLabel, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this._SourceField, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this._DestinationField, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(5);
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(674, 270);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // _DownloadButton
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this._DownloadButton, 2);
+            this._DownloadButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._DownloadButton.Enabled = false;
+            this._DownloadButton.Location = new System.Drawing.Point(174, 203);
+            this._DownloadButton.Name = "_DownloadButton";
+            this._DownloadButton.Size = new System.Drawing.Size(326, 59);
+            this._DownloadButton.TabIndex = 0;
+            this._DownloadButton.Text = "Download";
+            this._DownloadButton.UseVisualStyleBackColor = true;
+            this._DownloadButton.Click += new System.EventHandler(this._DownloadButton_Click);
+            // 
+            // _SourceLabel
+            // 
+            this._SourceLabel.AutoSize = true;
+            this._SourceLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._SourceLabel.Location = new System.Drawing.Point(10, 10);
+            this._SourceLabel.Margin = new System.Windows.Forms.Padding(5);
+            this._SourceLabel.Name = "_SourceLabel";
+            this._SourceLabel.Size = new System.Drawing.Size(156, 55);
+            this._SourceLabel.TabIndex = 1;
+            this._SourceLabel.Text = "Source URL";
+            this._SourceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // _DestinationLabel
+            // 
+            this._DestinationLabel.AutoSize = true;
+            this._DestinationLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._DestinationLabel.Location = new System.Drawing.Point(10, 75);
+            this._DestinationLabel.Margin = new System.Windows.Forms.Padding(5);
+            this._DestinationLabel.Name = "_DestinationLabel";
+            this._DestinationLabel.Size = new System.Drawing.Size(156, 55);
+            this._DestinationLabel.TabIndex = 2;
+            this._DestinationLabel.Text = "Destination File";
+            this._DestinationLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // _ProgressBar
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this._ProgressBar, 3);
+            this._ProgressBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._ProgressBar.Enabled = false;
+            this._ProgressBar.Location = new System.Drawing.Point(174, 144);
+            this._ProgressBar.Margin = new System.Windows.Forms.Padding(3, 9, 3, 9);
+            this._ProgressBar.Name = "_ProgressBar";
+            this._ProgressBar.Size = new System.Drawing.Size(492, 47);
+            this._ProgressBar.Step = 1;
+            this._ProgressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this._ProgressBar.TabIndex = 5;
+            // 
+            // _DestinationButton
+            // 
+            this._DestinationButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._DestinationButton.Location = new System.Drawing.Point(506, 73);
+            this._DestinationButton.Name = "_DestinationButton";
+            this._DestinationButton.Size = new System.Drawing.Size(160, 59);
+            this._DestinationButton.TabIndex = 6;
+            this._DestinationButton.Text = "Browse";
+            this._DestinationButton.UseVisualStyleBackColor = true;
+            this._DestinationButton.Click += new System.EventHandler(this._DestinationButton_Click);
+            // 
+            // _ProgressLabel
+            // 
+            this._ProgressLabel.AutoSize = true;
+            this._ProgressLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._ProgressLabel.Location = new System.Drawing.Point(10, 140);
+            this._ProgressLabel.Margin = new System.Windows.Forms.Padding(5);
+            this._ProgressLabel.Name = "_ProgressLabel";
+            this._ProgressLabel.Size = new System.Drawing.Size(156, 55);
+            this._ProgressLabel.TabIndex = 7;
+            this._ProgressLabel.Text = "Progress";
+            this._ProgressLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // _SourceField
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this._SourceField, 3);
+            this._SourceField.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._SourceField.Location = new System.Drawing.Point(174, 8);
+            this._SourceField.Name = "_SourceField";
+            this._SourceField.Size = new System.Drawing.Size(492, 59);
+            this._SourceField.TabIndex = 8;
+            this._SourceField.Text = "";
+            this._SourceField.TextChanged += new System.EventHandler(this._SourceField_TextChanged);
+            // 
+            // _DestinationField
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this._DestinationField, 2);
+            this._DestinationField.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._DestinationField.Location = new System.Drawing.Point(174, 73);
+            this._DestinationField.Name = "_DestinationField";
+            this._DestinationField.Size = new System.Drawing.Size(326, 59);
+            this._DestinationField.TabIndex = 9;
+            this._DestinationField.Text = "";
+            this._DestinationField.TextChanged += new System.EventHandler(this._DestinationField_TextChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 270);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MainForm";
+            this.Text = "HTTP(S) Downloader";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button _DownloadButton;
+        private System.Windows.Forms.Label _SourceLabel;
+        private System.Windows.Forms.Label _DestinationLabel;
+        private System.Windows.Forms.ProgressBar _ProgressBar;
+        private System.Windows.Forms.Button _DestinationButton;
+        private System.Windows.Forms.Label _ProgressLabel;
+        private System.Windows.Forms.RichTextBox _SourceField;
+        private System.Windows.Forms.RichTextBox _DestinationField;
+    }
+}
+
---
+++ HTTPDownloader/MainForm.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
+namespace HTTPDownloader
+{
+    public partial class MainForm : Form
+    {
+        private const long ProgressUpdateInterval = 1000;
+
+        private Color _DefaultTextFieldForeColor;
+        private bool _IsSourceValid;
+        private bool _IsDestinationValid;
+        private long _UpdateTimestamp;
+
+        public MainForm()
+        {
+            _IsSourceValid = false;
+            _IsDestinationValid = false;
+
+            InitializeComponent();
+
+            _DefaultTextFieldForeColor = _SourceField.ForeColor;
+        }
+
+        private double CalculatePercentage(long part, long total)
+        {
+            return ((double)part / (double)total) * 100.0;
+        }
+
+        private long GetCurrentTimestamp()
+        {
+            return DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+        }
+
+        private bool CheckDownloadConditions()
+        {
+            return (_IsSourceValid && _IsDestinationValid);
+        }
+        private void UpdateProgress(double progress)
+        {
+            if(progress == 100.0)
+            {
+                _ProgressLabel.Text = "Progress\nCompleted";
+            }
+            else
+            {
+                _ProgressLabel.Text = "Progress\n" + progress.ToString("0.0000") + "%";
+            }
+
+            _ProgressBar.Value = (int)progress;
+
+            _ProgressLabel.Update();
+            _ProgressBar.Update();
+        }
+
+        private void _SourceField_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                new Uri(_SourceField.Text);
+
+                _SourceField.ForeColor = _DefaultTextFieldForeColor;
+                _IsSourceValid = true;
+                _DownloadButton.Enabled = CheckDownloadConditions();
+            }
+            catch (Exception)
+            {
+                _SourceField.ForeColor = Color.Firebrick;
+                _IsSourceValid = false;
+            }
+        }
+
+        private void _DestinationField_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Path.GetFullPath(_DestinationField.Text);
+
+                _DestinationField.ForeColor = _DefaultTextFieldForeColor;
+                _IsDestinationValid = true;
+                _DownloadButton.Enabled = CheckDownloadConditions();
+            }
+            catch (Exception)
+            {
+                _DestinationField.ForeColor = Color.Firebrick;
+                _IsDestinationValid = false;
+            }
+        }
+
+        private void _DestinationButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog ofd = new SaveFileDialog();
+            DialogResult ofdResult;
+
+            ofdResult = ofd.ShowDialog();
+
+            if(ofdResult == DialogResult.OK)
+            {
+                _DestinationField.Text = ofd.FileName;
+                _DestinationField.ForeColor = _DefaultTextFieldForeColor;
+                _IsDestinationValid = true;
+                _DownloadButton.Enabled = CheckDownloadConditions();
+            }
+        }
+
+        private void _DownloadButton_Click(object sender, EventArgs e)
+        {
+            UpdateProgress(0.0);
+
+            _SourceField.Enabled = false;
+            _DestinationField.Enabled = false;
+            _DestinationButton.Enabled = false;
+            _DownloadButton.Enabled = false;
+            _ProgressBar.Enabled = true;
+            _UpdateTimestamp = GetCurrentTimestamp();
+
+            WebClient client = new WebClient();
+
+            client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(OnDownloadProgressChanged);
+            client.DownloadFileCompleted += new AsyncCompletedEventHandler(OnDownloadFileCompleted);
+            client.DownloadFileAsync(new Uri(_SourceField.Text), _DestinationField.Text);
+        }
+
+        private void OnDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            long currentTimestamp = GetCurrentTimestamp();
+
+            if ((currentTimestamp - _UpdateTimestamp) >= ProgressUpdateInterval)
+            {
+                double percentage = CalculatePercentage(e.BytesReceived, e.TotalBytesToReceive);
+
+                UpdateProgress(percentage);
+
+                _UpdateTimestamp = currentTimestamp;
+            }
+        }
+
+        private void OnDownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            UpdateProgress(100.0);
+
+            _SourceField.Enabled = true;
+            _DestinationField.Enabled = true;
+            _DestinationButton.Enabled = true;
+            _DownloadButton.Enabled = true;
+            _ProgressBar.Enabled = false;
+        }
+    }
+}
